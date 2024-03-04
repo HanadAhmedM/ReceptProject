dd1427fadc8a252a918eaf7652381c499b477248
HanadAhmedM <hanadahmed1414@gmail.com>
2024-03-04 21:30:39 +0100

    creating Recipe for details

---
+++ ReceptProject/Views/RecipieView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipieView.swift
+//  ReceptProject
+//
+//  Created by Hanad.Ahmed on 2024-03-04.
+//
+
+import SwiftUI
+
+struct RecipieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipieView()
+}
