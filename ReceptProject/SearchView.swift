e8a49f77b7805a2f503374bb5da6babc0e1328fd
HanadAhmedM <hanadahmed1414@gmail.com>
2024-03-04 15:32:43 +0100

    Creating searching side

---
+++ ReceptProject/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  ReceptProject
+//
+//  Created by Hanad.Ahmed on 2024-03-04.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
