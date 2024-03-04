e9382862a1d2ff08d8c4eaf69dea3ba83cdeb133
HanadAhmedM <hanadahmed1414@gmail.com>
2024-03-04 15:19:33 +0100

    Initial Commit

---
+++ ReceptProject/ReceptProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceptProjectApp.swift
+//  ReceptProject
+//
+//  Created by Hanad.Ahmed on 2024-03-04.
+//
+
+import SwiftUI
+
+@main
+struct ReceptProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
