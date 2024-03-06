58c1a43ddb72172dcf524d3c00b472b80dd6bd89
HanadAhmedM <hanadahmed1414@gmail.com>
2024-03-06 09:58:55 +0100

    Creating favorite Recipes View

---
+++ ReceptProject/Views/FavoriteRecipesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteRecipesView.swift
+//  ReceptProject
+//
+//  Created by Hanad.Ahmed on 2024-03-06.
+//
+
+import SwiftUI
+
+struct FavoriteRecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteRecipesView()
+}
