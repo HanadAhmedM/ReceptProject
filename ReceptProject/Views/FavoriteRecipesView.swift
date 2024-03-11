05aea7455ab4619b469a28b796c6e31fde91b9b5
jammingbadger <146725115+jammingbadger@users.noreply.github.com>
2024-03-11 20:32:58 +0100

    Card view mod in 'FavoriteRecipesView'

--- ReceptProject/Views/FavoriteRecipesView.swift
+++ ReceptProject/Views/FavoriteRecipesView.swift
@@ -7,51 +7,52 @@
 
 import SwiftUI
 
-struct FavoriteRecipesView: View {
+struct CardView: View {
     var body: some View {
-        //Matilda ska fixa detta, men får vara utkommenterat så länge
-       // HStack(spacing: 20) {
-               // Image(systemName: "chevron.left.circle")
-                    //.foregroundColor(Color.green)
-                   // .frame(alignment: .leading)
-               // Text("Favorite Recipes")
-               // .font(.largeTitle)
-                  //  .foregroundColor(.green)
-                  //  .frame(alignment: .center)
-           // }
-        
-        VStack{
-            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                HStack(spacing: 10){
-                    Image("1")
-                        .resizable()
-                        .cornerRadius(25)
-                        .frame(width: 100, height: 100)
-                    
-                    VStack(alignment: .leading) {
-                        Text("Lorem Ipsum")
-                            .font(.subheadline)
-                            .foregroundColor(Color(hex: 0x34A853))
-                        
-                        Text("Lorem ipsum dolor sit. Ipsum dolor sit.")
-                            .font(.caption2)
-                            .foregroundColor(Color(hex: 0xBFBFBF))
-                            .padding(3.0)
-                    }
-                    
-                    Spacer()
-                    
-                    Image(systemName: "xmark.circle.fill")
-                            .foregroundColor(Color.green)  // Set the foreground color of the heart to green
-                            .padding()
-                    }
-                    .background(Color(hex: 0xFFFFFF))
-              
-            }.cornerRadius(8.0)
+        HStack(spacing: 10) {
+            Image("1")
+                .resizable()
+                .cornerRadius(25)
+                .frame(width: 100, height: 100)
+            
+            VStack(alignment: .leading) {
+                Text("Lorem Ipsum")
+                    .font(.subheadline)
+                    .foregroundColor(Color(hex: 0x34A853))
+                
+                Text("Lorem ipsum dolor sit. Ipsum dolor sit.")
+                    .font(.caption2)
+                    .foregroundColor(Color(hex: 0xBFBFBF))
+                    .padding(3.0)
+            }
+            
+            Spacer()
+            
+            Image(systemName: "xmark.circle.fill")
+                .foregroundColor(Color.red)  // Set the foreground color of the heart to green
+                .padding()
         }
+        .padding(10)
+        .background(Color.white)
+        .cornerRadius(8.0)
+        .shadow(radius: 4)
     }
 }
 
-#Preview {
-    FavoriteRecipesView()
+struct FavoriteRecipesView: View {
+    var body: some View {
+        VStack {
+            List(0 ..< 5) { _ in
+                CardView()
+            }
+            .listStyle(PlainListStyle()) // Optional: Apply PlainListStyle to remove the default list styling
+        }
+        .padding() // Optional: Add padding to the VStack containing the list
+    }
+}
+
+struct FavoriteRecipesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteRecipesView()
+    }
 }
