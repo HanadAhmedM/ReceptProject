8ba6193ab511318c62ed00e2b149b526fab1c024
HanadAhmedM <hanadahmed1414@gmail.com>
2024-03-12 19:53:53 +0100

    favorite recipe

--- ReceptProject/ViewModels/FavoriteViewModel.swift
+++ ReceptProject/ViewModels/FavoriteViewModel.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import CoreData
+class FavoriteViewModel : ObservableObject{
+   
+    @Published var recipes: [Recept] = []
+    var container = Persistence.shared.container
+    init(){
+        self.fetchFavorite()
+    }
+    func fetchFavorite() {
+        let request = NSFetchRequest<Recept>(entityName: "Recept")
+
+        do {
+            recipes = try container.viewContext.fetch(request)
+            print("Fetch successful. Number of Recipe: \(recipes.count)")
+        } catch let error as NSError {
+            print("Error fetching notes: \(error.localizedDescription)")
+        }
+    }
+   
+    func addRecipe(id : Int,title : String,image: String){
+        if recipes.first(where: { $0.id == Int32(id) }) != nil {
+                   print("Recipe already exists. Do not add.")
+                   return
+               }
+        let newRecipe = Recept(context: container.viewContext)
+        newRecipe.id = Int32(id)
+        newRecipe.title = title
+        newRecipe.image = image
+       
+      
+        saveData()
+            fetchFavorite()  // Refresh the recipes array
+    }
+    func deleteFavorite(recipe: Recept){
+        container.viewContext.delete(recipe)
+            saveData()
+    }
+    func saveData(){
+        do {
+           try container.viewContext.save()
+        } catch let error{
+            print("error adding person \(error)")
+        }
+        fetchFavorite()
+    }
+    
+}
--- ReceptProject/ViewModels/Persistence.swift
+++ ReceptProject/ViewModels/Persistence.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+import CoreData
+
+struct Persistence {
+    static let shared = Persistence()
+
+    let container: NSPersistentContainer
+
+    init() {
+        container = NSPersistentContainer(name: "FavoriteRecipeModel")
+
+        container.loadPersistentStores { (description, error) in
+            if let error = error {
+                fatalError("Failed to load Core Data stack with error: \(error)")
+            } else {
+                print("Core Data stack loaded successfully")
+            }
+        }
+    }
+}
